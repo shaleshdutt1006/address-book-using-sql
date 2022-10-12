f6dfb8e4f8b1478f177baacf7cef01df13bdbf67
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-12 22:10:07 +0530

    Added: Sorting number of Contacts using FirstName in Addressbook Table

--- src/AddressBook.sql
+++ src/AddressBook.sql
@@ -1,10 +1,10 @@
 use address_book;
--- Editing existing contact using ContactId 4
-update contact set firstName="puneet",lastName="sharma",address="Jalandhar",state="Punjab",city="Jalandhar",
-zip=129822,phoneNumber=9468106649,email="puneet@gmail.com" where ContactId=4;
-
--- Searching existing contact using city and state
-select * from contact where city="delhi" and state="delhi";
 -- Counting number of Contacts in different cities
 select count(city) from contact ;
 select count(ContactId) from contact ;
+-- Sorted entry alphabetically using firstName
+SELECT * FROM contact ORDER BY firstName;
+-- Sorted entry using ContactId
+select * from contact order by ContactId;
+-- Sorted entry in descending order using ContactId
+select * from contact order by ContactId desc;
