a7494bdbe70a3456a66b1cf533376d6e07108cdb
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-12 23:09:05 +0530

    Added: Adding Person to family and friends type in Addressbook Table

--- src/AddressBook.sql
+++ src/AddressBook.sql
@@ -1,19 +1,10 @@
 use address_book;
--- Altering table and adding type of contact to the table
-alter table contact add type  varchar(255);
--- Updating contact and setting the type of contact whether it is family,friend or Colleague
-update contact set type="family" where ContactId=1 ;
-update contact set type="friends" where ContactId=2 ;
-update contact set type="family" where ContactId=3 ;
-update contact set type="Colleagues" where ContactId=4 ;
-update contact set type="Colleagues" where ContactId=5 ;
--- Count number of contacts by CountId
-select count(ContactId) from contact;
--- Count number of contacts of family type
-
 select count(type) from contact where type="family";
 -- Count number of contacts of friends type
 select count(type) from contact where type="friends";
 
 -- Count number of contact of different type and group by type
 SELECT type, COUNT(*) FROM contact GROUP BY type;
+-- Adding Person to family and friend type
+insert into contact (ContactId,firstName,lastName,address,state,city,zip,phoneNumber,email,type)values(6,"sumit","sharma","bandra Mumbai East","Pune","Maharashtra",156007,9468156647,"sumit@gmail.com","family");
+insert into contact (ContactId,firstName,lastName,address,state,city,zip,phoneNumber,email,type)values(7,"annu","sharma","bandra Mumbai East","Pune","Maharashtra",156007,9468156647,"annu@gmail.com","friend");
