c2ec8b3ad90822bff7d1373bd4b9f51e0e32b112
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-12 22:19:34 +0530

    Added: Adding type of Contacts family,friends in Addressbook Table

--- src/AddressBook.sql
+++ src/AddressBook.sql
@@ -1,7 +1,12 @@
 use address_book;
--- Counting number of Contacts in different cities
-select count(city) from contact ;
-select count(ContactId) from contact ;
+-- Altering table and adding type of contact to the table
+alter table contact add type  varchar(255);
+-- Updating contact and setting the type of contact whether it is family,friend or Colleague
+update contact set type="family" where ContactId=1 ;
+update contact set type="friends" where ContactId=2 ;
+update contact set type="family" where ContactId=3 ;
+update contact set type="Colleagues" where ContactId=4 ;
+update contact set type="Colleagues" where ContactId=5 ;
 -- Sorted entry alphabetically using firstName
 SELECT * FROM contact ORDER BY firstName;
 -- Sorted entry using ContactId
