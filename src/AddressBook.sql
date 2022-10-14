3466323ba3dad2917cdfccce68dd5bab7dd1940e
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-14 16:20:07 +0530

    Refactored: Some changes in ER diagram of address book service

--- src/AddressBook.sql
+++ src/AddressBook.sql
@@ -1,10 +1,8 @@
 use address_book;
-select count(type) from contact where type="family";
--- Count number of contacts of friends type
-select count(type) from contact where type="friends";
-
--- Count number of contact of different type and group by type
-SELECT type, COUNT(*) FROM contact GROUP BY type;
--- Adding Person to family and friend type
+insert into contact (firstName,lastName,address,state,city,zip,phoneNumber,email,type)values("amit","sharma","model town","haryana","kurukshetra",135006,9468459936,"amit@gmail.com","family");
+insert into contact values(2,"sumit","sharma","bandra Mumbai East","Pune","Maharashtra",156007,9468156647,"sumit@gmail.com","friend");
+insert into contact values(3,"amit", "dutt", "mahatam buddh nagar", "kaithal", "haryana", 54654, "845165489165","amit@gmail.com","Colleague");
+insert into contact values(4,"ankita", "sharma", "andheri", "mumbai", "maharashtra", 56565, "94516558498","ankita@gmail.com","friend");
+insert into contact values(5,"anita", "sharma", "model", "delhi", "delhi", 26556, "9468156647","anita@gmail.com","Colleague");
 insert into contact (ContactId,firstName,lastName,address,state,city,zip,phoneNumber,email,type)values(6,"sumit","sharma","bandra Mumbai East","Pune","Maharashtra",156007,9468156647,"sumit@gmail.com","family");
 insert into contact (ContactId,firstName,lastName,address,state,city,zip,phoneNumber,email,type)values(7,"annu","sharma","bandra Mumbai East","Pune","Maharashtra",156007,9468156647,"annu@gmail.com","friend");
